463da87428fc0d6e56a9ab7bc1455ef0d9398a95
Austin Sousa <asousa@stanford.edu>
2017-03-21 21:55:22 -0700

    parallel statistics stuff

---
+++ python/GLD_file_tools.py
@@ -0,0 +1,329 @@
+#import threading
+import time
+#from Queue import Queue
+import logging
+import numpy as np
+import os
+import datetime
+#import matplotlib
+#matplotlib.use('GTKAgg')
+#from matplotlib import pyplot as plt
+#from mpl_toolkits.basemap import Basemap
+import datetime
+#import ephem
+import math
+#from StreamReader import StreamReader
+#import json
+import fnmatch
+
+
+
+
+# ----------------------------------------------------------
+# A set of modules to quickly search and parse GLD entries
+# ----------------------------------------------------------
+#
+# Version 1.1:  Added logic to deal with overlapping files
+#               (asking for flashes spanning two files)
+#               6.8.2016 APS
+#
+# Version 1.2:  Added try/catch blocks around datetime_from_row
+#               added line[0] == 0 condition for happy rows
+#               (Attempting to bulletproof reads on junky files)
+#               6.14.2016 APS                
+# ----------------------------------------------------------
+class GLD_file_tools(object):
+  def __init__(self,filepath, prefix='GLD'):
+    self.GLD_root = filepath
+    self.file_list = []
+    self.suffix = '.dat'
+    self.prefix = prefix
+    #self.refresh_directory()
+
+  
+
+  def refresh_directory(self):
+    ''' Get file list within directory '''
+    logging.info('Refreshing file list')
+    self.file_list = []
+    # Get datetime objects for each file in directory:
+    for root, dirs, files in os.walk(self.GLD_root):
+        if file.endswith(self.suffix):
+          print file
+          self.file_list.append([(datetime.datetime.strptime(file,self.prefix + '-%Y%m%d%H%M%S.dat')),
+                            (os.path.join(root,file))])
+          #filepaths.append(os.path.join(root,file))
+          #startdates.append(datetime.datetime.strptime(file,'GLD-%Y%m%d%H%M%S.dat'))
+    
+    self.file_list.sort(key=lambda tup: tup[0])
+
+    #logging.info('Refreshed file list')
+    #print file_list
+  # def get_filename(self, t):
+  #   '''returns a subdirectory string -- just here to avoid
+  #     a ton of redundant string parsing'''
+  #   folder = datetime.datetime.strftime(t,'%Y-%m-%d')
+  #   files = os.listdir(os.path.join(self.GLD_root,folder))
+  #   file_list = []
+  #   for file in files:
+  #    if file.endswith(self.suffix):
+  #     print file
+  #     file_list.append([(datetime.datetime.strptime(file,self.prefix + '-%Y%m%d%H%M%S.dat')),
+  #                         (os.path.join(self.GLD_root,file))])
+
+  #   file_list.sort(key=lambda tup: tup[0])
+  #   return file_list[-1] # newest file
+
+
+
+  def get_file_at(self,t):
+    ''' t: datetime object
+           Finds the last file in self.file_list with time less than t
+    '''    
+    #startfile = filter(lambda row: row[0] <= t, self.file_list)[-1]
+    #startfiles = filter(lambda row: row[0] <= t, self.file_list)
+    
+    # if len(startfiles) > 0:
+    #   return startfiles[-1]
+    # else:
+    #   logging.info("No files found!")
+    #   return 
+    folder = datetime.datetime.strftime(t,'%Y-%m-%d')
+    
+    if not os.path.exists(os.path.join(self.GLD_root,folder)):
+      return None, None
+
+    files = os.listdir(os.path.join(self.GLD_root,folder))
+    file_list = []
+    for file in files:
+     if file.endswith(self.suffix):
+      #print file
+      file_list.append([(datetime.datetime.strptime(file,self.prefix + '-%Y%m%d%H%M%S.dat')),
+                          (os.path.join(self.GLD_root,folder,file))])
+
+    file_list.sort(key=lambda tup: tup[0])
+    #logging.info(file_list)
+
+    # Select only files which start earlier than our target time:
+    file_list = [f for f in file_list if t >= f[0]]
+
+    return file_list[-1]  # Last one in the list
+    # return file_list[-1] # newest file
+
+
+
+  def load_flashes(self, t, dt = datetime.timedelta(0,0,0,0,1,0)):
+    '''filepath: GLD file to sift thru
+       t: datetime object to search around
+       dt: datetime.timedelta 
+
+       returns: A list of tuples: <time ob
+    '''
+    # print t
+    # print type(t)
+    
+    filetime,filepath = self.get_file_at(t)
+    
+    tprev = t - dt
+
+    rows =  []
+    times = []
+    if filetime is not None:
+      print "section 1"
+      # return None, None
+    # else:
+
+      # print "t:",t
+      # print "tprev:",tprev
+      # print "filetime:",filetime
+      #buff_size = 100000 # bytes
+      # Binary search thru entries:
+      imax = np.floor(os.path.getsize(filepath)).astype('int')
+      imin = 0
+
+      thefile = open(filepath,'r')
+
+      
+      # Find closest index to target time:
+      # print "recursing t_ind"
+      t_ind = self.recursive_search_kernel(thefile, t, imin, imax)
+      print t_ind
+      print self.datetime_from_row(self.parse_line(thefile,t_ind))
+      
+      # Find closest index to window time:
+      # print "recursing t_prev"
+      tprev_ind = self.recursive_search_kernel(thefile,tprev,imin,imax)
+      # print tprev_ind
+      print self.datetime_from_row(self.parse_line(thefile,tprev_ind))
+
+
+      # print "t_ind, tprev_ind (section 1)",t_ind, tprev_ind
+      if (t_ind is not None) and (tprev_ind is not None):
+        # return None, None
+        # Load rows between tprev_ind and t_ind:
+        while (thefile.tell() < t_ind):
+          curr_line = self.parse_line(thefile,thefile.tell())
+
+          newtime = self.datetime_from_row(curr_line)
+          # datetime_from_row will return None if line is unhappy
+          if newtime is not None:
+            rows.append(curr_line)
+            times.append(newtime)
+      
+      
+      # In the case that tprev runs over the start of the file (asking for flashes at 12:01...)
+      # This could be more-elegant but I'm just repeating the previous code
+    print tprev, filetime
+    if (filetime is None) or (tprev < filetime):
+      # print "section 2"
+      filetime,filepath = self.get_file_at(tprev)
+
+
+      if filetime is not None:
+        # print "doing overlap"
+        imax = np.floor(os.path.getsize(filepath)).astype('int')
+        imin = 0
+
+        thefile = open(filepath,'r')
+        t_ind = imax
+        # Find closest index to window time:
+        tprev_ind = self.recursive_search_kernel(thefile,tprev,imin,imax)
+        #print self.datetime_from_row(self.parse_line(thefile,tprev_ind))
+        
+        # print "t_ind, t_prev:",t_ind, tprev_ind
+
+        rows_prev = []
+        times_prev= []
+        # if (t_ind is None) or (tprev_ind is None):
+        #   return None, None
+        # Load rows between tprev_ind and t_ind:
+        if (t_ind is not None) and (tprev_ind is not None):
+          while (thefile.tell() < t_ind):
+            curr_line = self.parse_line(thefile,thefile.tell())
+            newtime = self.datetime_from_row(curr_line)
+
+            # datetime_from_row will return None if line is unhappy
+
+            if newtime is not None:
+              rows_prev.append(curr_line)
+              times_prev.append(newtime)
+
+            # rows_prev.append(curr_line)
+            # times_prev.append(self.datetime_from_row(curr_line))
+        
+        
+        rows[0:0] = rows_prev
+        times[0:0] = times_prev
+
+    logging.info(" Found " + str(len(rows)) + " entries between " + str(tprev) + " and " + str(t))
+    if len(rows) > 0:
+      return np.asarray(rows), np.asarray(times) 
+    else:
+      return None, None
+       
+  def recursive_search_kernel(self, thefile, target_time, imin, imax, n= 0 ):
+    ''' Recursively searches thefile (previously open) for the closest entry
+        to target_time (datetime object)
+    '''
+    imid = imin + ((imax - imin)/2)
+    #imid = ((imax-imin)/2)
+    l = self.parse_line(thefile,imid)
+    if l is None:
+      return None
+    # print l
+    y,m,d,H,M,S = l[0:6].astype('int')
+    curr_time = datetime.datetime(y,m,d,H,M,S)
+    
+    if n > 50:
+      print 'max recursions!'
+      return None
+    if abs(imin - imax) <= 200:
+      # print n, imin, imax, imid, imax-imin, curr_time
+      return imin
+    else:
+      if curr_time >= target_time:
+        # print 'too high: ',imin, imax, imid, imax-imin, curr_time
+        imax = imid
+        #imax -=1
+      else:
+        # print 'too low: ',imin, imax, imid, imax-imin, curr_time
+        imin = imid
+        #imin += 1
+      # Uncomment this to show recursion (hella sweet)  
+      # print imin, imax, imax-imin, curr_time
+      return self.recursive_search_kernel(thefile,target_time,imin,imax,n+1)
+      
+  def parse_line(self, thefile, theindex, n=0):
+    '''
+    Returns a parsed line; recursively skips forward if line isn't full-length
+    '''
+    thefile.seek(theindex,0)
+    line = thefile.readline()
+    vec = line.split('\t')
+
+    # print "index:",theindex
+    if n > 50:
+      # print "failed to find an entry"
+      logging.info("Failed to find an entry")
+      return None
+
+    if (len(vec)==25) and (vec[0] == '0'):
+      # print vec[0]
+      try:
+        return np.array(vec[1:11],'float')
+      except:
+        return self.parse_line(thefile=thefile,theindex=thefile.tell(), n=(n+1))
+
+    else:
+      # if (thefile.tell() == theindex):
+        # At end of file -- jump back a few lines, use that value
+      # print vec  
+      #   logging.info("Hit end of file hit -- jumping backwards " + str(n) + ' ' + str(theindex))
+      # thefile.seek(thefile.tell() - 200,0) # Healthy line is ~96 long  
+
+      # Something we would like to do: If we're less than a full line away
+      # from the end of the file, back up and try for the whole line.
+      # This would help on some of the broken days (multiple weird files).
+      # For example: datetime(2015,2,8,6,0,0)  -- aps 3.2017
+
+      # if thefile.tell() > thefile.totalsize - 96:
+        # Scroll backward
+      # thefile.seek(thefile.tell() - 200,0) # Healthy line is ~96 long    
+      return self.parse_line(thefile=thefile,theindex=thefile.tell(), n=(n+1))
+
+  def datetime_from_row(self, row):
+    # print row
+    y,m,d,H,M,S,n = row[0:7].astype('int')
+    micros = n/1000
+    try:
+      return datetime.datetime(y,m,d,H,M,S,micros)
+    except:
+      return None
+
+
+
+# ---------------------------
+# Main block
+# ---------------------------
+
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.DEBUG,
+                      format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+                      )  
+
+  GLD_root = 'alex/array/home/Vaisala/feed_data/GLD'
+  t = datetime.datetime(2015,04,02,16,20,00)
+  #startfile, startfile_time = get_file_at(t)
+  #startfile ='alex/array/home/Vaisala/feed_data/GLD/2015-03-26/GLD-201503260223.dat'
+
+  print 'initializing'
+
+  G = GLD_file_tools(GLD_root)
+
+  print 'doing'
+  #G.load_flashes(t)
+
+  for hours in xrange(0,24):
+    for mins in xrange(0,60):
+      f = G.load_flashes(datetime.datetime(2015,04,02, hours, mins,0))
+    
---
+++ python/GLD_statistics.py
@@ -0,0 +1,417 @@
+from mpi4py import MPI
+import commands
+from partition import partition 
+
+import matplotlib
+matplotlib.use('agg')
+
+import numpy as np
+import pandas as pd
+import pickle
+import gzip
+
+from joblib import Parallel, delayed
+import multiprocessing
+from index_helpers import load_Kp
+import matplotlib.pyplot as plt
+import os
+import itertools
+import random
+import os
+import time
+import datetime as datetime
+import types
+import scipy.io
+import matplotlib.gridspec as gridspec
+
+
+
+from scipy import stats
+import xflib
+import logging
+import math
+from mpl_toolkits.mplot3d import Axes3D
+
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+xf = xflib.xflib(lib_path='/shared/users/asousa/WIPP/3dWIPP/python/libxformd.so')
+from scipy.integrate import nquad
+
+from GLD_file_tools import GLD_file_tools
+
+
+# Input settings:
+
+pwr_db_path = '/shared/users/asousa/WIPP/lightning_power_study/outputs/inp_pwr_db_1deg_bins.pklz'
+output_path = '/shared/users/asousa/WIPP/lightning_power_study/outputs/GLDstats'
+fig_path = os.path.join(output_path, 'figures')
+dump_path = os.path.join(output_path,'data')
+
+
+lookback_time = datetime.timedelta(hours=3)
+
+# The range of times we'll do:
+start_day = datetime.datetime(2015,1,1,0,0,0)
+stop_day = datetime.datetime(2016,1,1,0,0,0)
+
+
+
+# ------------ Start MPI -------------------------------
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+host = commands.getoutput("hostname")
+nProcs = 1.0*comm.Get_size()
+
+
+if rank == 0:
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    if not os.path.exists(fig_path):
+        os.mkdir(fig_path)
+    if not os.path.exists(dump_path):
+        os.mkdir(dump_path)
+        
+
+# --------------------- Definitions -------------------------------
+
+# Convert the Matlab coastline datafile to geomagnetic coordinates:
+def get_coast_mag(itime):
+    xf = xflib.xflib(lib_path='/shared/users/asousa/WIPP/3dWIPP/python/libxformd.so')
+    coastlines = scipy.io.loadmat('/shared/users/asousa/WIPP/lightning_power_study/python/coastlines.mat')
+
+    coast_lat_mag = np.zeros(len(coastlines['lat']))
+    coast_lon_mag = np.zeros(len(coastlines['long']))
+
+    for ind, (lat, lon) in enumerate(zip(coastlines['lat'], coastlines['long'])):
+        if np.isnan(lat) or np.isnan(lon):
+            coast_lat_mag[ind] = np.nan
+            coast_lon_mag[ind] = np.nan
+        else:
+            tmpcoords = [1, lat[0], lon[0]]
+            tmp_mag = xf.rllgeo2rllmag(tmpcoords, itime)
+            coast_lat_mag[ind] = tmp_mag[1]
+            coast_lon_mag[ind] = tmp_mag[2]
+
+    # Loop around for -180 + 180 ranges
+    coast_lat_mag = np.concatenate([coast_lat_mag, coast_lat_mag[coast_lon_mag > 180]])
+    coast_lon_mag = np.concatenate([coast_lon_mag, (coast_lon_mag[coast_lon_mag > 180] - 360)])
+
+    # Toss in some NaNs to break up the continents
+    for ind in range(len(coast_lat_mag) -1):
+        if ((np.abs(coast_lat_mag[ind+1] - coast_lat_mag[ind]) > 5) or
+           (np.abs(coast_lon_mag[ind+1] - coast_lon_mag[ind]) > 5)):
+            coast_lat_mag[ind] = np.nan
+            coast_lon_mag[ind] = np.nan
+
+    return coast_lat_mag, coast_lon_mag
+
+def data_grid_at(in_time):
+#     print np.shape(times_to_do)
+    print "loading flashes at ", in_time
+    data_grid = []
+#     for in_time in times_to_do:
+
+    Kpm = Kpmax[Kpmtimes == in_time]
+    Kp_cur = Kp[np.where(Ktimes == in_time)[0]]
+
+    flashes, flash_times = gld.load_flashes(in_time, lookback_time)
+    print np.shape(flashes)
+    if flashes is not None:
+        for flash, flashtime in zip(flashes, flash_times):
+
+            glat = flash[7]
+            glon = flash[8]
+            I    = flash[9]
+
+            # Get location in geomagnetic coordinates
+            mloc = xf.rllgeo2rllmag([1.0, glat, glon], flashtime)
+
+            # Get MLT:
+            mlt = xf.lon2MLT(flashtime, mloc[2])
+
+            data_grid.append([mloc[1], mloc[2], mlt, I, Kpm, Kp_cur])
+        data_grid = np.array(data_grid)
+        return data_grid
+    else:
+        return None
+
+
+# Output power space:
+def analyze_flashes(data_grid, in_time):
+
+    outdata = dict()
+    dg2 = np.array(data_grid)
+
+    # Quantize data into lat, lon, and MLT bins:
+    dg2[:,0] = np.digitize(dg2[:,0],gridlats)
+    dg2[:,1] = np.digitize(np.mod(dg2[:,1], 360.0) - 180.0, gridlons)
+    dg2[:,2] = (dg2[:,2] > 6) & (dg2[:,2] <= 18)   # Is day?
+
+    hist_bins = np.arange(0,24.5, 0.5)
+    flash_map = np.zeros([len(gridlats), len(gridlons)])
+    cur_map   = np.zeros([len(gridlats), len(gridlons)])
+    pwr_map   = np.zeros([len(gridlats), len(gridlons)])
+    mlt_hist, _  = np.histogram(data_grid[:,2], hist_bins)
+
+    # Bin total current by lat and lon
+    day_bins = np.zeros([len(gridlats), len(gridlons)])
+    nite_bins= np.zeros([len(gridlats), len(gridlons)])
+
+    
+    for row in dg2:
+        # Power stencils are proportional to current squared;
+        # GLD currents are in kA
+        if row[2]:
+            day_bins[int(row[0]), np.mod(int(row[1]), 360)] += pow(row[3]*1e3, 2.0)
+        else:
+            nite_bins[int(row[0]), np.mod(int(row[1]), 360)] += pow(row[3]*1e3, 2.0)
+        
+        # Flash count histogram
+        flash_map[int(row[0]), np.mod(int(row[1]), 360)] += 1
+        
+        # 2d current histogram
+        flash_map[int(row[0]), np.mod(int(row[1]), 360)] += np.abs(row[3]*1e3)
+
+
+
+    day_todo = np.where(day_bins > 0)
+    nite_todo = np.where(nite_bins > 0)
+
+    for isday in [False, True]:
+        if isday:
+            todo = np.where(day_bins > 0)
+        else:
+            todo = np.where(nite_bins > 0)
+        for latind, lonind in zip(todo[0], todo[1]):
+            if (np.abs(gridlats[latind]) >= stencil_lats[0]) & (np.abs(gridlats[latind]) <= stencil_lats[-1]):
+                key = (np.abs(np.round(gridlats[latind])), 0)
+                if key in inp_pwr_dict:
+                    stencil = inp_pwr_dict[key]
+                    if isday:
+                        pwr = day_bins[latind, lonind]
+                    else:
+                        pwr = nite_bins[latind, lonind]
+                    latleft = int(latind - cell_lat_offset)
+                    latright = int(latind + cell_lat_offset-1)
+                    lonleft = int(lonind - cell_lon_offset)
+                    lonright =int(lonind + cell_lon_offset-1)
+
+                    if lonleft < 0:
+                        # Wrap around left:
+                        pwr_map[latleft:latright, 0:lonright] += \
+                                stencil[:, np.abs(lonleft):]*pwr
+                        pwr_map[latleft:latright, (len(gridlons) - np.abs(lonleft)):] += \
+                                stencil[:,0:np.abs(lonleft)]*pwr
+                    elif lonright >= len(gridlons):
+                        # wrap around right:
+                        pwr_map[latleft:latright, lonleft:len(gridlons)] += \
+                            stencil[:,0:len(gridlons) - lonleft]*pwr
+                        pwr_map[latleft:latright, 0:np.abs(lonright) - len(gridlons)] += \
+                            stencil[:,len(gridlons) - lonleft:]*pwr
+                    else:
+                        pwr_map[latleft:latright, lonleft:lonright] += stencil*pwr
+
+    outdata['pwr_map'] = pwr_map
+    outdata['flash_map'] = flash_map
+    outdata['cur_map'] = cur_map
+    outdata['mlt_hist'] = mlt_hist
+    outdata['in_time'] = in_time
+    return outdata
+
+def plot_pwr_data(outdata):
+    # --------------- Latex Plot Beautification --------------------------
+    fig_width = 12 
+    fig_height = 6
+    fig_size =  [fig_width+1,fig_height+1]
+    params = {'backend': 'ps',
+              'axes.labelsize': 14,
+              'font.size': 14,
+              'legend.fontsize': 14,
+              'xtick.labelsize': 14,
+              'ytick.labelsize': 14,
+              'text.usetex': False,
+              'figure.figsize': fig_size}
+    plt.rcParams.update(params)
+    # --------------- Latex Plot Beautification --------------------------
+    
+    pwr_map = outdata['pwr_map']
+    flash_map = outdata['flash_map']
+    cur_map = outdata['cur_map']
+    mlt_hist = outdata['mlt_hist']
+    in_time = outdata['in_time']
+
+    clims=[0, 4] # log space
+
+    # Plot flash location with integrated current vs lat and lon:
+    fig = plt.figure()
+    gs = gridspec.GridSpec(2,3,width_ratios=[0.5,10,0.25],
+                       height_ratios=[10,1])
+    # gs.update(left=0.05, right=0.48, wspace=0.05)
+    ax1 = plt.subplot(gs[0:-1,1:-1])  # main figure
+    ax0 = plt.subplot(gs[0:-1,0])   #
+    ax2 = plt.subplot(gs[-1,1:-1])
+    cbar_ax = plt.subplot(gs[:, -1])
+    pwr_bylat = np.sum(pwr_map, axis=1)/lookback_time.total_seconds()
+    pwr_bylon = np.sum(pwr_map, axis=0)/lookback_time.total_seconds()
+    
+    logpwr = np.log10(pwr_map/3600./3.0)  # Energy per second, in log space.
+    logpwr[np.isinf(logpwr)] = -10
+
+
+    p = ax1.pcolorfast(gridlons, gridlats, logpwr, cmap = plt.get_cmap('viridis'))
+    p.set_clim(clims)
+
+    cb = plt.colorbar(p, cax=cbar_ax)
+
+    cb.set_label('Average Energy Flux [J/sec]')
+    cticks = np.arange(clims[0],clims[1] + 1)
+    cb.set_ticks(cticks)
+    cticklabels = ['$10^{%d}$'%k for k in cticks]
+    cb.set_ticklabels(cticklabels)
+
+
+
+    coast_lat_mag, coast_lon_mag = get_coast_mag(in_time)
+    ax1.plot(coast_lon_mag, coast_lat_mag, 'w')
+    ax1.set_xlim([-180, 180])
+    ax1.set_ylim([-90, 90])
+    p.set_clim([0,4])
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax0.plot(pwr_bylat,gridlats)
+    ax0.set_ylim([-90,90])
+    ax0.set_xticks([])
+    ax0.set_ylabel('Latitude (magnetic)')
+    ax2.plot(gridlons, pwr_bylon)
+    ax2.set_xlim([-180,180])
+    ax2.set_yticks([])
+    ax2.set_xlabel('Longitude (magnetic)')
+
+    ax1.set_title( in_time.isoformat() )
+    fig.tight_layout()
+
+    return fig, ax0, ax1, ax2
+
+# ----------------- MAIN RUN BLOCK --------------------------
+# Load input power database:
+with gzip.open('/shared/users/asousa/WIPP/lightning_power_study/outputs/inp_pwr_db_1deg_bins.pklz','rb') as f:
+    inp_pwr_dict = pickle.load(f)
+
+tmp = np.array(sorted([k for k in inp_pwr_dict.keys() if not isinstance(k, basestring)]))
+
+stencil_lats = np.unique(tmp[:,0]); stencil_MLTs = np.unique(tmp[:,1])
+
+cellsize = inp_pwr_dict['cellsize']
+cell_lat_offset = np.round(inp_pwr_dict['lat_spread']/inp_pwr_dict['cellsize'])
+cell_lon_offset = np.round(inp_pwr_dict['lon_spread']/inp_pwr_dict['cellsize'])
+
+gridlats = np.arange(-90, 90, cellsize)
+gridlons = np.arange(-180, 180, cellsize)
+gridmlts = np.linspace(0,24, 9)
+
+# Get Kp data
+Ktimes, Kp = load_Kp()
+Ktimes = [k + datetime.timedelta(minutes=90) for k in Ktimes]  # 3-hour bins; the original script labeled them in the middle of the bin
+Ktimes = np.array(Ktimes)
+Kp = np.array(Kp)
+
+# Get Kpmax -- max value of Kp over the last 24 hours (8 bins):
+Kpmax = np.max([Kp[0:-8],Kp[1:-7],Kp[2:-6], Kp[3:-5], Kp[4:-4],Kp[5:-3],Kp[6:-2], Kp[7:-1], Kp[8:]],axis=0)
+Kpmtimes = Ktimes[8:]
+
+
+# lookback_time = np.unique(np.diff(Kpmtimes))[0] # This should be 3 hours
+
+num_cores = multiprocessing.cpu_count()
+
+
+# Lightning data getter
+GLD_path = '/home/asousa/GLD_mount/'
+gld = GLD_file_tools(GLD_path, prefix='GLD')
+
+
+
+# Do one file per day, since we're just parallelizing over cores.
+# 8 kp per day, 8 cores, bam.
+# def job(intime):
+#     datagrid = data_grid_at(intime)
+#     if datagrid is not None:
+#         return analyze_flashes(datagrid, intime)
+#     else:
+#         return None
+
+
+
+# days_to_do = [start_day + datetime.timedelta(days=x) for x in range((stop_day - start_day).days + 1)]
+# day_pairs = zip(days_to_do[0:-1], days_to_do[1:])
+
+# for day1, day2 in day_pairs:
+#     times_to_do = Ktimes[(Ktimes > day1) & (Ktimes <= day2)]
+#     # print times_to_do
+#     datae = Parallel(n_jobs=num_cores)(delayed(job)(t) for t in times_to_do)
+
+#     for datum in datae:
+#         if datum is not None:
+#             fig, ax0, ax1, ax2 = plot_pwr_data(datum)
+#             figname = os.path.join(fig_path, datum['in_time'].strftime('%d_%m_%Y_%H_%M') +'.png')
+#             fig.savefig(figname, ldpi=300)
+#             plt.close('all')
+
+#             filename = os.path.join(dump_path, datum['in_time'].strftime('%d_%m_%Y_%H_%M') + '.pklz')
+#             with gzip.open(filename, 'wb') as f:
+#                 pickle.dump(datum,f)
+
+# days_to_do = [start_day + datetime.timedelta(days=x) for x in range((stop_day - start_day).days + 1)]
+# day_pairs = zip(days_to_do[0:-1], days_to_do[1:])
+
+
+if rank == 0:
+    tasklist = Ktimes[(Ktimes > start_day) & (Ktimes <= stop_day)]
+    chunks = partition(tasklist, nProcs)
+else:
+    tasklist = None
+    chunks = None
+
+tasklist = comm.bcast(tasklist, root=0)
+chunks   = comm.bcast(chunks, root=0)
+
+nTasks  = 1.0*len(tasklist)
+nSteps = np.ceil(nTasks/nProcs).astype(int)
+
+if (rank < len(chunks)):
+    print "Subprocess %s on %s: doing %d tasks"%(rank, host, len(chunks[rank]))
+
+
+    for intime in chunks[rank]:
+        datagrid = data_grid_at(intime)
+        if datagrid is not None:
+            datum = analyze_flashes(datagrid, intime)
+
+            if datum is not None:
+                fig, ax0, ax1, ax2 = plot_pwr_data(datum)
+                figname = os.path.join(fig_path, datum['in_time'].strftime('%m_%d_%Y_%H_%M') +'.png')
+                fig.savefig(figname, ldpi=300)
+                plt.close(fig)
+
+                filename = os.path.join(dump_path, datum['in_time'].strftime('%m_%d_%Y_%H_%M') + '.pklz')
+                with gzip.open(filename, 'wb') as f:
+                    pickle.dump(datum,f)
+
+
+
+
+#     datae = Parallel(n_jobs=num_cores)(delayed(job)(t) for t in times_to_do)
+
+#     for datum in datae:
+#         if datum is not None:
+#             fig, ax0, ax1, ax2 = plot_pwr_data(datum)
+#             figname = os.path.join(fig_path, datum['in_time'].strftime('%d_%m_%Y_%H_%M') +'.png')
+#             fig.savefig(figname, ldpi=300)
+#             plt.close('all')
+
+#             filename = os.path.join(dump_path, datum['in_time'].strftime('%d_%m_%Y_%H_%M') + '.pklz')
+#             with gzip.open(filename, 'wb') as f:
+#                 pickle.dump(datum,f)
+
+
+
